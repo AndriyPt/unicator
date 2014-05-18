56759c15521017ca86a86684f62017d31229c02f
andriypt@gmail.com <andriypt@gmail.com@d39736d8-62e7-d5aa-638e-f285ffea8f20>
2014-05-18 09:34:20 +0000

    First version with main classes

---
+++ src/main/java/org/unicator/application/FilesCleaner.java
@@ -0,0 +1,9 @@
+package org.unicator.application;
+
+public class FilesCleaner {
+
+    public static void main(String[] args) {
+    }
+
+}
+
---
+++ src/main/java/org/unicator/application/FilesIndexer.java
@@ -0,0 +1,9 @@
+package org.unicator.application;
+
+public class FilesIndexer {
+
+    public static void main(String[] args) {
+    }
+
+}
+
