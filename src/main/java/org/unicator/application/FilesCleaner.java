5bf22c2ea89b4bb11577d9629eb5e1acd71ea380
andriypt@gmail.com <andriypt@gmail.com@d39736d8-62e7-d5aa-638e-f285ffea8f20>
2014-05-18 12:50:43 +0000

    Added first test run version

--- src/main/java/org/unicator/application/FilesCleaner.java
+++ src/main/java/org/unicator/application/FilesCleaner.java
@@ -1,9 +1,59 @@
 package org.unicator.application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.unicator.comparator.IndexInfoComparator;
+import org.unicator.model.IndexInfo;
+import org.unicator.util.IndexUtils;
+import org.unicator.util.JavaUtils;
+
 public class FilesCleaner {
 
     public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Application usage: <program> <base path> <duplicates path> \n");
+            System.exit(1);
+        }
+
+        final String basePath = args[0];
+        JavaUtils.checkDirectoryExists(basePath);
+
+        final String duplicatesPath = args[1];
+        JavaUtils.checkDirectoryExists(duplicatesPath);
+
+        final File duplicatesDirectory = new File(duplicatesPath);
+        final String duplicatesAbsolutePath = duplicatesDirectory.getAbsolutePath();
+
+        try {
+            final List<IndexInfo> indeces = IndexUtils.loadIndex(basePath);
+
+            final Collection<File> allPossibleDuplicates = FileUtils.listFiles(duplicatesDirectory, null, true);
+
+            for (File file : allPossibleDuplicates) {
+                final IndexInfo currentFile = IndexUtils.getIndexInfoFromFile(duplicatesAbsolutePath, file);
+                final int index = Collections.binarySearch(indeces, currentFile, new IndexInfoComparator());
+                if (index >= 0) {
+                    final File entryFromIndex = new File(basePath, indeces.get(index).getFilePath());
+                    if (FileUtils.contentEquals(entryFromIndex, file)) {
+                        JavaUtils.printToConsole("File " + file.getAbsolutePath() + " is duplicate of "
+                            + entryFromIndex.getAbsolutePath() + " and will be deleted");
+                        // file.delete();
+                    }
+
+                }
+
+            }
+
+        }
+        catch (IOException e) {
+            JavaUtils.printToConsole(e.getMessage());
+        }
     }
 
 }
-
